a347d01ba68a5d6c358fd6ccb64810198886414c
merencia <lucas.merencia@gmail.com>
2018-03-16 13:47:41 -0300

    testing

--- src/node-gmt.js
+++ src/node-gmt.js
@@ -1,46 +1,61 @@
 'use strict';
-module.exports = (function(){
+module.exports = (function () {
+    
+    /**
+     * Retrieve the date timestamp at GMT+00:00
+     * @param {Date} date 
+     * @returns {Number} timestamp
+     */
+    function retrieveUTCTime(date) {
+        var currentOffset = date.getTimezoneOffset() * 60000;
+        if (currentOffset > 0) {
+            return date.getTime() + currentOffset;
+        }
+        return date.getTime() - currentOffset;
+    }
+    
+    /**
+     * Creates a GMT
+     * @param {String} entry GMT representation e.g: GMT-03:00  
+     * @param {Number} entry GMT offset in minutes (Date.getTimezoneOffset return)
+     */
     function GMT(entry) {
-        if(typeof entry === 'string'){
+        if (typeof entry === 'string') {
             var reg = /^GMT(\+|\-)(\d{2})\:(\d{2})$/;
             var match = reg.exec(entry);
             this.signal = match[1];
             this.hours = parseInt(match[2]);
             this.minutes = parseInt(match[3]);
+            this.offsetInMinutes = this.hours * 60 + this.minutes;
         } else {
-            this.signal = this.offsetInMinutes < 0 ? '+' : '-';
+            this.signal = entry < 0 ? '+' : '-';
             this.offsetInMinutes = Math.abs(entry);
-            this.hours = this.offsetInMinutes / 60;
+            this.hours = Math.floor(this.offsetInMinutes / 60);
             this.minutes = this.offsetInMinutes % 60;
         }
     }
-    
-    GMT.prototype.getUTCDiff = function(){
+
+    /**
+     * @returns {Number} difference beetwen the gmt and GMT+00:00 in miliseconds  
+     */
+    GMT.prototype.getUTCDiff = function () {
         var offset = (this.hours * 60 + this.minutes) * 60000;
-        if(this.signal === '-'){
+        if (this.signal === '-') {
             return offset * -1;
         }
         return offset;
     }
-    
-    GMT.prototype.getUTCTime = function(date){
-        var currentOffset = date.getTimezoneOffset() * 60000;
-        if(currentOffset > 0) {
-            return date.getTime() + currentOffset;
-        } 
-        return date.getTime() - currentOffset;
-    }
-    
-    GMT.prototype.toString = function(){
+
+    GMT.prototype.toString = function () {
         return 'GMT' + this.signal + this.hours.toString().padStart(2, '0') + ':' + this.minutes.toString().padStart(2, '0')
     }
-    
-    GMT.prototype.relativeDate = function(date){
-        var dateUtcTime = this.getUTCTime(date);
+
+    GMT.prototype.relativeDate = function (date) {
+        var dateUtcTime = retrieveUTCTime(date);
         return new Date(dateUtcTime + this.getUTCDiff());
     }
-    
-    GMT.current = function (){
+
+    GMT.getCurrent = function () {
         var offsetInMinutes = new Date().getTimezoneOffset();
         return new GMT(offsetInMinutes);
     }
--- test/node-gmt-test.js
+++ test/node-gmt-test.js
@@ -5,8 +5,72 @@ var GMT = require('../src/node-gmt');
 
 describe('node-gmt', () => {
     it('should retrieve local gmt', ()=>{
-        var currentGmt = GMT.current();
+        var currentGmt = GMT.getCurrent();
         var offset = new Date().getTimezoneOffset() * -60000;
         expect(offset).equal(currentGmt.getUTCDiff());
+    });
+
+    it('should create a negative GMT by yours representation string', ()=>{
+        var gmt = new GMT('GMT-03:00');
+        expect(gmt.hours).equal(3);
+        expect(gmt.minutes).equal(0);
+        expect(gmt.signal).equal('-');
+        expect(gmt.offsetInMinutes).equal(180);
+        expect(gmt.getUTCDiff()).equal(-180 * 60000);
+    });
+
+    it('should create a positive GMT by yours representation string', ()=>{
+        var gmt = new GMT('GMT+04:30');
+        expect(gmt.hours).equal(4);
+        expect(gmt.minutes).equal(30);
+        expect(gmt.signal).equal('+');
+        expect(gmt.offsetInMinutes).equal(270);
+        expect(gmt.getUTCDiff()).equal(270 * 60000);
+    });
+
+    it('should create a negative GMT by offset', ()=>{
+        var gmt = new GMT(180);
+        expect(gmt.hours).equal(3);
+        expect(gmt.minutes).equal(0);
+        expect(gmt.signal).equal('-');
+        expect(gmt.offsetInMinutes).equal(180);
+        expect(gmt.getUTCDiff()).equal(-180 * 60000);
+        expect(gmt.toString()).equal('GMT-03:00');
+    });
+
+    it('should create a positive GMT by offset', ()=>{
+        var gmt = new GMT(-270);
+        expect(gmt.hours).equal(4);
+        expect(gmt.minutes).equal(30);
+        expect(gmt.signal).equal('+');
+        expect(gmt.offsetInMinutes).equal(270);
+        expect(gmt.getUTCDiff()).equal(270 * 60000);
+        expect(gmt.toString()).equal('GMT+04:30');
+    });
+
+    it('should retrieve the relative date at GMT+00:00', ()=>{
+        var gmt = new GMT('GMT+00:00');
+        var date = new Date('August 1, 2018 00:00:00 GMT-04:30');
+        var relativeDate = gmt.relativeDate(date);
+        expect(relativeDate.getHours()).equal(4);
+        expect(relativeDate.getMinutes()).equal(30);
+    });
+
+    it('should retrieve the relative date at GMT+02:00', ()=>{
+        var gmt = new GMT('GMT+02:00');
+        var date = new Date('August 1, 2018 00:00:00 GMT-04:30');
+        var relativeDate = gmt.relativeDate(date);
+        expect(relativeDate.getHours()).equal(6);
+        expect(relativeDate.getMinutes()).equal(30);
+    });
+
+    it('should retrieve the relative date at GMT-6:00', ()=>{
+        var gmt = new GMT('GMT-06:00');
+        var date = new Date('August 1, 2018 00:00:00 GMT-04:30');
+        var relativeDate = gmt.relativeDate(date);
+        expect(relativeDate.getHours()).equal(22);
+        expect(relativeDate.getMinutes()).equal(30);
+        expect(relativeDate.getDate()).equal(31);
+        expect(relativeDate.getMonth()).equal(6);
     });
 });
